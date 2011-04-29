6a1aaf2742e583a75c551f1bf0e94f101dcbaa3c
tchklovski@gmail.com <tchklovski@gmail.com>
2011-04-29 00:48:24 -0700

    updated .gitignore

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject clj_eliza "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject eliza_clj "0.0.1-SNAPSHOT"
+  :description "Collaborative Eliza -- a chatbot anyone can contribute to"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [org.clojure/clojure-contrib "1.2.1"]
+		 [compojure "0.6.2"]
+		 ])
--- src/clj_eliza/utils.clj
+++
@@ -1,7 +0,0 @@
-(ns clj_eliza.utils)
-
-(defn first-true [fn col]
-  "return the first true value of applying fn to col"
-  (first (filter identity (map fn col))))
-
-
